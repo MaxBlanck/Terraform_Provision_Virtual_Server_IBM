2942ec30cb35a2cea2098edcaaeeb4253a7be2d2
Max-Blanck <max.blanck@ibm.com>
2018-06-25 13:01:54 +0200

    Webspehere Docker Image

--- files/install.sh
+++ files/install.sh
@@ -12,7 +12,15 @@ echo y | sudo yum install /tmp/docker-ce-18.03.0.ce-1.el7.centos.x86_64.rpm
 sudo systemctl start docker
 
 #Download Docker Image
-sudo docker run hello-world
+#sudo docker run hello-world
+sudo docker pull ibmcom/websphere-traditional:profile
 
-#Instantiate Image in Container on Port 8081
-docker run -d --restart=always -p 8081:8081 hello-world:latest
+#Instantiate Image in Container
+#docker run -d --restart=always -p 8081:8081 hello-world:latest
+docker run --name test -h test -p 9043:9043 -p 9443:9443 -d ibmcom/websphere-traditional:profile
+
+#Retrieve admin password with ID: wsadmin
+echo "Password Admin Websphere:"
+docker exec test cat /tmp/PASSWORD
+
+#Open console under https://localhost:9043/ibm/console/login.do?action=secure
